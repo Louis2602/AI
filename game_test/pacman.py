b261fe5483c73ea3306698259ab388e43ecd0f97
Louis2602 <trantunglam135790@gmail.com>
2023-07-24 11:23:16 +0700

    update game pacman

---
+++ game_test/pacman.py
@@ -0,0 +1,54 @@
+from setup import Hero, Cookie, Powerup, Wall, Ghost, GameRenderer, GameObject
+from utils import GhostBehaviour, draw_path
+from controller import PacmanGameController
+from utils import translate_maze_to_screen
+
+
+if __name__ == "__main__":
+    unified_size = 32
+    pacman_game = PacmanGameController()
+    size = pacman_game.size
+    game_renderer = GameRenderer(size[0] * unified_size, size[1] * unified_size)
+
+    for y, row in enumerate(pacman_game.numpy_maze):
+        for x, column in enumerate(row):
+            if column == 0:
+                game_renderer.add_wall(Wall(game_renderer, x, y, unified_size))
+
+    # Draw path
+    draw_path(pacman_game, game_renderer, unified_size, GameObject, Wall)
+
+    for cookie_space in pacman_game.cookie_spaces:
+        translated = translate_maze_to_screen(cookie_space)
+        cookie = Cookie(
+            game_renderer,
+            translated[0] + unified_size / 2,
+            translated[1] + unified_size / 2,
+        )
+        game_renderer.add_cookie(cookie)
+
+    for powerup_space in pacman_game.powerup_spaces:
+        translated = translate_maze_to_screen(powerup_space)
+        powerup = Powerup(
+            game_renderer,
+            translated[0] + unified_size / 2,
+            translated[1] + unified_size / 2,
+        )
+        game_renderer.add_powerup(powerup)
+
+    for i, ghost_spawn in enumerate(pacman_game.ghost_spawns):
+        translated = translate_maze_to_screen(ghost_spawn)
+        ghost = Ghost(
+            game_renderer,
+            translated[0],
+            translated[1],
+            unified_size,
+            pacman_game,
+            pacman_game.ghost_colors[i % 4],
+        )
+        game_renderer.add_ghost(ghost)
+
+    pacman = Hero(game_renderer, unified_size, unified_size, unified_size)
+    game_renderer.add_hero(pacman)
+    game_renderer.set_current_mode(GhostBehaviour.CHASE)
+    game_renderer.tick(120)
---
+++ game_test/utils.py
@@ -0,0 +1,59 @@
+from enum import Enum
+
+
+class Direction(Enum):
+    DOWN = -90
+    RIGHT = 0
+    UP = 90
+    LEFT = 180
+    NONE = 360
+
+
+class ScoreType(Enum):
+    COOKIE = 10
+    POWERUP = 50
+    GHOST = 400
+
+
+class GhostBehaviour(Enum):
+    CHASE = 1
+    SCATTER = 2
+
+
+def translate_screen_to_maze(in_coords, in_size=32):
+    return int(in_coords[0] / in_size), int(in_coords[1] / in_size)
+
+
+def translate_maze_to_screen(in_coords, in_size=32):
+    return in_coords[0] * in_size, in_coords[1] * in_size
+
+
+def draw_path(pacman_game, game_renderer, unified_size, GameObject, Wall):
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    _from = (1, 1)
+    _to = (24, 14)
+    path_array = pacman_game.p.get_path(_from[1], _from[0], _to[1], _to[0])
+
+    print(path_array)
+    # [(1, 2), (1, 3), (1, 4), (1, 5), (2, 5), (3, 5), (4, 5), (5, 5), (6, 5), (6, 6), (6, 7) ...
+
+    white = (255, 255, 255)
+    for path in path_array:
+        game_renderer.add_game_object(
+            Wall(game_renderer, path[0], path[1], unified_size, white)
+        )
+
+    from_translated = translate_maze_to_screen(_from)
+    game_renderer.add_game_object(
+        GameObject(
+            game_renderer, from_translated[0], from_translated[1], unified_size, red
+        )
+    )
+
+    to_translated = translate_maze_to_screen(_to)
+    game_renderer.add_game_object(
+        GameObject(
+            game_renderer, to_translated[0], to_translated[1], unified_size, green
+        )
+    )
